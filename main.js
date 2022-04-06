3336be9531a9c8eb5e331e91ebff5dbc470afa59
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-04-06 18:13:06 +0200

    00002 - increase the zoom value

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function init() {
   new ol.Map({
     view: new ol.View({
       center: [0, 0],
-      zoom: 2,
+      zoom: 5,
     }),
     layers: [
       new ol.layer.Tile({ source: new ol.source.OSM() }), //Open Street Map
