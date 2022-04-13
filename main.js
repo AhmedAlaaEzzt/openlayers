bd95e611e33a4c3e8b03b246f06f82dcde38bb21
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-04-13 14:12:10 +0200

    00007 - create the demo

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 function init() {
-  new ol.Map({
+  const map = new ol.Map({
     view: new ol.View({
       center: [0, 0],
       zoom: 5,
     }),
-    layers: [
-      new ol.layer.Tile({ source: new ol.source.OSM() }), //Open Street Map
-    ],
+    layers: [],
     target: "js-map",
   });
+
+  const osmLayer = new ol.layer.Tile({ source: new ol.source.OSM() });
+  map.addLayer(osmLayer);
 }
 
-init()
+init();
