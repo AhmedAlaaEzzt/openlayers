a79944f35b9a49e42d732d5ef11f305b51312de9
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-07-02 18:19:43 +0200

    00010 - adding overlay

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 function init() {
-  new ol.Map({
+  const map = new ol.Map({
     view: new ol.View({
       center: [0, 0],
       zoom: 5,
     }),
     layers: [
       new ol.layer.Tile({ source: new ol.source.OSM() }), //Open Street Map
+      new ol.layer.Vector({source: new ol.source.Vector({
+        url: './data/vector/map.geojson',
+        format: new ol.format.GeoJSON()
+      })})
     ],
     target: "js-map",
   });
+
+
+  const overlayContainer = document.querySelector(".overlay-container");
+  const fearureName = document.getElementById("fearure-name");
+  const fearureAdditionalInfo = document.getElementById(
+    "fearure-additional-info"
+  );
+
+  const overlayLayer = new ol.Overlay({
+		element: overlayContainer,
+	});
+
+	map.addOverlay(overlayLayer);
+
+  	
+	  // Vector Feature Popup Logic
+	map.on("click", (e) => {
+    overlayLayer.setPosition(undefined);
+
+    overlayLayer.setPosition(e.coordinate);
+  })
+
+
 }
 
 init()
